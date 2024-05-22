94316c8d866dd2abba2fa2b502dc907d8261c110
soulsilver <122255949+soul0silver@users.noreply.github.com>
2024-05-22 09:11:40 +0700

    Update User.java

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,4 @@
-package com.example.demo.model;
+ưupackage com.example.demo.model;
 
 import com.example.demo.model.Emp;
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -24,3 +24,40 @@ public class User {
     @ToString.Exclude
     private Emp emp;
 }
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Constraint(validatedBy = EnumValidatorImpl.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+public @interface EnumValidator {
+
+    Class<? extends Enum<?>> enumClazz();
+
+    String message() default "Value is not valid";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+    import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EnumValidatorImpl implements ConstraintValidator<EnumValidator, CharSequence> {
+
+    List<String> acceptedValues;
+
+    @Override
+    public void initialize(EnumValidator constraintAnnotation) {
+        acceptedValues = Stream.of(constraintAnnotation.enumClazz().getEnumConstants())
+                .map(Enum::name)
+                .collect(Collectors.toList());
